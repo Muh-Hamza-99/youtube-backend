7648674a3d2c7e051b9d1a7b16e35bd623b033e4
muhammad-hamza <Muh-Hamza-99>
2022-06-12 15:47:10 +0300

    Sending back JSON tokens instead of user

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -2,10 +2,13 @@ const JWT = require("jsonwebtoken");
 
 const User = require("./../models/User");
 
+const signToken = id => JWT.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TOKEN_EXPIRES_IN });
+
 const signup = async (req, res) => {
     const { username, email, password } = req.body;
     const newUser = await User.create({ username, email, password });
-    res.status(201).json({ status: "success", user: newUser });
+    const token = signToken(newUser._id);
+    res.status(201).json({ status: "success", token });
 };
 
 const login = async (req, res) => {
@@ -13,7 +16,8 @@ const login = async (req, res) => {
     if (!email || !password) return;
     const user = await User.findOne({ email }).select("+password");
     if (!user || await user.correctPassword(password, user.password)) return;
-    res.status(200).json({ status: "success", user });
+    const token = signToken(user._id);
+    res.status(200).json({ status: "success", token });
 };
 
 module.exports = {
