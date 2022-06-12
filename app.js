5ddec3f34d048005ca3419c45ca2b9674d024574
muhammad-hamza <Muh-Hamza-99>
2022-06-12 14:54:11 +0300

    Using Mongoose ORM

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 require("dotenv").config({ path: "./config.env" });
 
+const mongoose = require("mongoose")
+
 const express = require("express");
 const app = express();
+
+const DB = process.env.MONGO_URI.replace("<PASSWORD>", process.env.MONGO_PASSWORD);
+
+mongoose.connect(DB)
+    .then(() => console.log("Successfully connected to DB!"))
+    .catch(error => console.log(`Something went wrong...${error}.`));
 
 const PORT = process.env.PORT || 3000;
 
