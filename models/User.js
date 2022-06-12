38f80d94757bf3ba29cef502550d06feb87d18f3
muhammad-hamza <Muh-Hamza-99>
2022-06-12 15:41:25 +0300

    Hashing passwords and comparing passwords middleware and method

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -32,6 +34,17 @@ const userSchema = new mongoose.Schema({
         default: [],
     },
 });
+
+userSchema.pre("save", async function(next) {
+    if (this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.methods.correctPassword = async function(providedPassword, userPassword) {
+    return await bcrypt.compare(providedPassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
