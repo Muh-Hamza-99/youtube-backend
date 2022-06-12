a707cabe83728b30d683c15ae25a55b8a1451d5e
muhammad-hamza <Muh-Hamza-99>
2022-06-12 15:09:09 +0300

    User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please enter your username!"],
+        unique: [true, "Account with this username is already taken!"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email!"],
+        unique: [true, "Account with this email is already taken!"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+        minlength: 8,
+    },
+    videos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        },
+    ],
+    subscribers: {
+        type: Array,
+        default: [],
+    },
+    subscribedChannels: {
+        type: Array, 
+        default: [],
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
