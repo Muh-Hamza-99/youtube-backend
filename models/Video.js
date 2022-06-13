0c0c512080244da392f5f716d5e69d4e92f05821
muhammad-hamza <Muh-Hamza-99>
2022-06-13 09:43:02 +0300

    Video schema and model

---
+++ models/Video.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    name: {
+        type: String,
+        required: [true, "Please enter a name for the video!"],
+    },
+    videoPath: {
+        type: String,
+        required: [true, "Video path is required to upload video!"],
+        unique: [true, "Video path must be unique!"],
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    dislikes: {
+        type: Number,
+        default: 0,
+    },
+    views: {
+        type: Array,
+        default: [],
+    },
+    comments: {
+        type: Array,
+        default: [],
+    },
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = Video;
