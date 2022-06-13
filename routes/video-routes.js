6ba05fde8452f41890aca01dd2c137206c54c1b3
muhammad-hamza <Muh-Hamza-99>
2022-06-13 11:05:21 +0300

    Testing video upload middleware

--- routes/video-routes.js
+++ routes/video-routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const videoUpload = require("../middleware/video-upload");
 const protect = require("../middleware/protect");
 
-router.get("/test", protect, (req, res) => res.json({ message: "It worked" }));
+router.get("/test", protect, videoUpload.single("video"), (req, res) => res.json({ message: "It worked" }));
 
 module.exports = router;
