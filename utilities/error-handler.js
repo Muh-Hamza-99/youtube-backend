58daf2c83c0c25f1465637e14c26c40f1613f6d4
muhammad-hamza <Muh-Hamza-99>
2022-06-13 18:06:01 +0300

    Handling special errors from JWT and MongoDB

--- utilities/error-handler.js
+++ utilities/error-handler.js
@@ -21,6 +21,27 @@ const sendErrorProduction = (error, res) => {
     };
 };
 
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = err => {
+    const value = err.errmsg.match(/[^\1\\]|\\.)*?\1/);
+    const message = `Duplicate field value: ${value}. Please use another value!`
+    return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join(". ")}`;
+    return new AppError(message, 400);
+};
+
+const handleJWTError = () => new AppError("Invalid token. Please log in again!", 401);
+
+const handleJWTExpiredError = () => new AppError("Your token has expired! Please log in again!", 401);
+
 const globalErrorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
