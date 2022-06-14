f44848e5151b56f7b405dbcf888f000e5433d5e2
muhammad-hamza <Muh-Hamza-99>
2022-06-14 17:30:48 +0300

    Sending token as cookie and JSON response

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -7,11 +7,22 @@ const catchAsync = require("./../utilities/catch-async");
 
 const signToken = id => JWT.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TOKEN_EXPIRES_IN });
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+    };
+    res.cookie("jwt", token, cookieOptions);
+    user.password = undefined;
+    res.status(statusCode).json({ status: "success", token });
+};
+
 const signup = catchAsync(async (req, res, next) => {
     const { username, email, password } = req.body;
     const newUser = await User.create({ username, email, password });
-    const token = signToken(newUser._id);
-    res.status(201).json({ status: "success", token });
+    createSendToken(newUser, 201, res);
 });
 
 const login = catchAsync(async (req, res, next) => {
@@ -19,8 +30,7 @@ const login = catchAsync(async (req, res, next) => {
     if (!email || !password) return next(new AppError("Required credentials not provided!", 400));
     const user = await User.findOne({ email }).select("+password");
     if (!user || await user.correctPassword(password, user.password)) return next(new AppError("Invalid credentials provided!", 400));
-    const token = signToken(user._id);
-    res.status(200).json({ status: "success", token });
+    createSendToken(user, 201, res);
 });
 
 module.exports = {
