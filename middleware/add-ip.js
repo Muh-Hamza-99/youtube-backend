bfb9e42c1e363fe256191ff8c78365c9f55fee69
muhammad-hamza <Muh-Hamza-99>
2022-06-14 16:37:36 +0300

    Adding IP addresses based on views length and finding its index

--- middleware/add-ip.js
+++ middleware/add-ip.js
@@ -9,7 +9,12 @@ const addIP = catchAsync(async (req, res, next) => {
     const { videoID, IP } = req.params;
     const video = await Video.findById(videoID);
     if (!video) return next(new AppError("No video with the provided ID!", 400));
-    await Video.findByIdAndUpdate(videoID, { $push: { views: IP } }, { runValidators: true, new: true });  
+    if (!video.views.length) await Video.findByIdAndUpdate(videoID, { $push: { views: IP } }, { runValidators: true, new: true });
+    else {
+        const index = video.views.indexOf(IP);
+        if (index === -1) await Video.findByIdAndUpdate(videoID, { $push: { views: IP } }, { runValidators: true, new: true });
+        next();
+    };
     next();  
 });
 
