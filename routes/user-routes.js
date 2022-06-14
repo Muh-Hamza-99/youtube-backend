97d346670ed48a9aaec5bd21c6cc3963c069a06c
muhammad-hamza <Muh-Hamza-99>
2022-06-14 17:32:30 +0300

    Update password controller and route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
     signup,
     login,
+    updatePassword
 } = require("../controllers/auth-controllers");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.patch("/updateMyPassword", updatePassword);
 
 module.exports = router;
