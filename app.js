2b7d11f7ab1639984e3031ab271da5ca01a21391
muhammad-hamza <Muh-Hamza-99>
2022-06-14 18:28:35 +0300

    Creating separate main file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 require("dotenv").config({ path: "./config.env" });
 
-const mongoose = require("mongoose")
-
 const express = require("express");
 const app = express();
 
@@ -52,12 +50,4 @@ app.all("*", (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-const DB = process.env.MONGO_URI.replace("<PASSWORD>", process.env.MONGO_PASSWORD);
-
-mongoose.connect(DB)
-    .then(() => console.log("Successfully connected to DB!"))
-    .catch(error => console.log(`Something went wrong...${error}.`));
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}...`))
+module.exports = app;
